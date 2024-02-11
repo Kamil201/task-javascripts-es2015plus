4760c696be8257158343f6625aeff01ea6eca70d
Kamil Lewandowski <kamlew555@gmail.com>
2024-02-11 12:28:39 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+module.exports = {
+	// ...
+	devServer: {
+		static: "./assets/css/style.css",
+	},
+};
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// importuję bibliotekę [path] z [node.js]
+module.exports = {
+	entry: "./assets/js/script.js",
+	// definiuję plik wejściowy
+	output: {
+		path: path.resolve(__dirname, "build"),
+		// definiuję ścieżkę wyjściową
+		filename: "app.min.js",
+		// definiuję nazwę pliku wyjściowego
+	},
+
+	module: {
+		rules: [],
+		// obecnie brak dodatkowych ustawień
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./index.html",
+			// wskazuję plik źródłowy
+			filename: "index.html",
+			// określam nazwę dla pliku
+		}),
+	],
+};
+// eksportuję ustawienia dla webpack
